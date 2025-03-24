c81c9418d02c326d9df8966077076375d189f7e4
NadiaMejz <103553832+NadiaMejz@users.noreply.github.com>
2025-03-24 16:13:25 +0100

    add new container

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@
     {
         var vessel1 = new ContainerVessel(12,100,1000,new List<Container>());
         var contener = new L(100.0, 2.5, 50, 4, 200);
-        
+        var contener2 = new C(100.0, 2.5, 50, 4, 200,24);
+
         vessel1.LoadContainerOntoShip(contener);
       
         
